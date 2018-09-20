b688857bb445850cc7d8bb409600133285915532
Holland Graves <zacharyledoux13@gmail.com>
2018-09-20 02:10:35 -0600

    made a newDeck() function that creates a new deck by nesting two slices of suits and values of cards and iterating through them, appending each iteration to the cards slice, and returning the cards slice. Then, in the main.go folder, calling the .print() method on cards, which stores the value of the newDeck() function call

--- deck.go
+++ deck.go
@@ -9,6 +9,21 @@ import (
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Clubs", "Spades", "Hearts", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
