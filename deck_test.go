68f26a7f1f7cdfd1d195c8c31038d4956dfc14e2
Holland Graves <zacharyledoux13@gmail.com>
2018-09-28 22:56:14 -0600

    added somemore into the test function

--- deck_test.go
+++ deck_test.go
@@ -4,12 +4,16 @@ import (
 	"testing"
 )
 
+// these following shorthand codes are placeholders for the following specific value
+// e.g.
+//
+// %d is the placeholder for an integer
+// %s is the placeholder for a string
+// %+v is the placeholder for every value an object has
+
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
-		// %d is a
-		// %s
-		// %+v
 		t.Errorf("Error: deck of cards is not 52, it is %d", len(d))
 	}
 }
