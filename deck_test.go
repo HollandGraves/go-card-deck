adfef7af73c2e2758e92ef29dcd0b82f6e9b4414
Holland Graves <zacharyledoux13@gmail.com>
2018-09-28 22:52:46 -0600

    added somemore into the test function

--- deck_test.go
+++ deck_test.go
@@ -1,7 +1,15 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
-
+	d := newDeck()
+	if len(d) != 52 {
+		// %d is a
+		// %s
+		// %+v
+		t.Errorf("Error: deck of cards is not 52, it is %d", len(d))
+	}
 }
